0e233294503888e35a5cb3e695d7ddbff39276b5
Mahdiwav <mahdifaghani82@gmail.com>
2025-11-29 21:01:42 +0330

    add alien move

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,7 +83,19 @@ class AlienInvasion:
             self.bullets.add(new_bullet)
 
     def _update_aliens(self):
+        self._check_fleet_edges()
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_bullets(self):
         self.bullets.update()
@@ -91,6 +103,12 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        self.ship_speed = 2
+        self.ship_speed = 5
 
         self.bullet_speed = 2.0
         self.bullet_width = 3
@@ -14,3 +14,5 @@ class Settings:
         self.bullets_allowed = 3
 
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
